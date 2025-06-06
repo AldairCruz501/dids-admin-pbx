154515743fba5e1a4df91e7837fe0a2c37d8d117
Acruzbautista501 <acruz.pbxhosting@gmail.com>
2025-06-06 13:58:03 -0600

    Actualizacion de api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: '/api',
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
   }
 })
 
