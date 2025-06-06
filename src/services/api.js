379139787d50d8366f3fec46663d352fd4661b89
Acruzbautista501 <acruz.pbxhosting@gmail.com>
2025-06-06 12:46:02 -0600

    Actualizacion de vista DIDS

--- src/services/api.js
+++ src/services/api.js
@@ -4,16 +4,13 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    Accept: 'application/json'
+    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
   }
 })
 
-api.interceptors.request.use(config => {
-  const token = import.meta.env.VITE_API_TOKEN
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-  return config
-})
+console.log(import.meta.env.VITE_API_BASE_URL)
+console.log(import.meta.env.VITE_API_TOKEN)
 
 export default api
